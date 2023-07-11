b13e141f942fb2de589e0b1ef1eaeca025e788bd
raghad <ragadrashwan@gmail.com>
2023-07-11 03:19:11 +0300

    modifying the view index.blade file and the web.php file

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+//Route::get('/', function () {
+  //  return view('index');
+//});
+
+
+Route::get('/', [\App\Http\Controllers\HomeCntroller::class, 'index'])
+    ->name( name: 'home');
