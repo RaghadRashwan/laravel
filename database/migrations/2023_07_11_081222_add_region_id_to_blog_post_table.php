fa36dadfa427788314c7898c8be9d25a69f7cbf1
raghad <ragadrashwan@gmail.com>
2023-07-12 10:37:14 +0300

    droping the blog_bost table

---
+++ database/migrations/2023_07_11_081222_add_region_id_to_blog_post_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        
+          Schema::dropIfExists('blog_posts');
+          //$table->foreignId(column: 'category_id')->constrained();
+        //$table->foreignId('category_id')->constrained();
+
+           // $table->foreign('user_id')
+                //  ->references('id')
+                 // ->on('categories')
+                //  ->onDelete('cascade');
+        //});
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('blog_post', function (Blueprint $table) {
+            //
+        });
+    }
+};
