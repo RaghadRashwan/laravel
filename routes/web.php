5710c383675b64c2a69a6ae7400f69e92a038557
raghad <ragadrashwan@gmail.com>
2023-07-12 12:50:35 +0300

    modifying index.blade file and the web.php file to enable the user accessing a specific post by clicking into the image of the post

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\HomeCntroller::class, 'index'])
     ->name( name: 'home');
+
+    Route::get('Posts/{post}', [\App\Http\Controllers\PostController::class, 'show'])
+    ->name( name: 'Posts.show');
