10c6e2cc841591fa6d4b1168890f996e164b6948
raghad <ragadrashwan@gmail.com>
2023-07-25 14:16:26 +0300

    modifying the view files of posts and category to apply the @can function and also the AppServiceProvider to add the Gate::before() method

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -14,7 +14,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <a href="{{ route('categories.create') }}" class="block max-w-sm p-6 bg-blue-500 text-white font-bold uppercase rounded-lg shadow hover:bg-blue-600">Add new category</a>
+                    @can('create category')
+                    <a href="{{ route('categories.create') }}" class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg shadow hover:bg-blue-600">Add new category</a>
+                    @endcan
                     <br /><br />
                     <div class="container">
                         <div class="flex flex-wrap -mx-4 justify-center">
@@ -27,12 +29,17 @@
                                         <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                             <h2 class="mb-4 text-lg font-semibold tracking-tight text-gray-900 dark:text-white">{{ $category->name }}</h2>
                                             <div class="flex justify-between items-center">
+                                                @can('edit category')
                                                 <a href="{{ route('categories.edit', $category) }}" class="text-sm font-bold text-blue-500 hover:underline">Edit</a>
+                                                @endcan
+
+                                                @can('delete category')
                                                 <form method="POST" action="{{ route('categories.destroy', $category) }}" style="display: inline-block;">
                                                     @csrf
                                                     @method('DELETE')
                                                     <button type="submit" class="text-sm font-bold text-red-500 hover:underline" onclick="return confirm('Are you sure?')">Delete</button>
                                                 </form>
+                                                @endcan
                                             </div>
                                         </div>
                                     </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,51 +4,58 @@
             {{ __('Posts') }}
         </h2>
     </x-slot>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Blog Home - Start Bootstrap Template</title>
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
-        <style>
-            .card {
-                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-            }
-        </style>
-    </head>
-    <div class="py-12">
+
+    
+
+    <div class="py-12 flex justify-center">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <a href="{{ route('posts.create') }}" class="btn btn-primary">Add new post</a>
+                    @can('create post')
+                    <a href="{{ route('posts.create') }}" class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg shadow hover:bg-blue-600">
+                        Add new post
+                    </a>
+                    @endcan
                     <br /><br />
                     <div class="container">
-                        <div class="row justify-content-center">
+                        <div class="flex flex-wrap -mx-4 justify-center">
                             <!-- Blog entries-->
-                            <div class="col-lg-8">
+                            <div class="w-full lg:w-2/3 px-4">
                                 <!-- Nested row for non-featured blog posts-->
-                                <div class="row">
+                                <div class="flex flex-wrap -mx-4">
                                     @foreach($posts as $post)
-                                    <div class="col-lg-6">
+                                    <div class="w-full lg:w-1/2 px-4 mb-8">
                                         <!-- Blog post-->
-                                        <div class="card mb-4">
-                                            <img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="...">
-                                            <div class="card-body">
-                                                <div class="small text-muted">{{ $post->created_at }}</div>
-                                                <h2 class="card-title h4">{{ $post->title }}</h2>
-                                                <h3 class="card-title h4">{{ $post->category->name }}</h3>
-                                                <p class="card-text">{{ $post->post_text }}</p>
-                                                <a class="btn btn-primary" href="{{ route('posts.show', $post->id) }}" style="background-color: grey; border-color: grey;">Read more →</a>
-                                                <a class="btn btn-primary" href="{{ route('posts.edit', $post) }}">Edit</a>
+                                        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                            <a href="#">
+                                                <img class="rounded-t-lg" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="...">
+                                            </a>
+                                            <div class="p-5">
+                                                <a href="#">
+                                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $post->title }}</h5>
+                                                </a>
+                                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $post->post_text }}</p>
+                                                <a href="{{ route('posts.show', $post->id) }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                    Read more
+                                                     <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                                    </svg>
+                                                </a>
+                                                @can('edit post')
+                                                <a href="{{ route('posts.edit', $post) }}" class="inline-flex items-center px-3 py-2 mt-2 text-sm font-medium text-white bg-green-500 rounded-lg shadow hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                                                    Edit
+                                                </a>
+                                                @endcan
+
+                                                @can('delete post')
                                                 <form method="POST" action="{{ route('posts.destroy', $post) }}" style="display: inline-block;">
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                                    <button type="submit" class="inline-flex items-center px-3 py-2 mt-2 text-sm font-medium text-white bg-red-500 rounded-lg shadow hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" onclick="return confirm('Are you sure?')">
+                                                        Delete
+                                                    </button>
                                                 </form>
+                                                @endcan
                                             </div>
                                         </div>
                                     </div>
@@ -63,6 +70,7 @@
     </div>
     </html>
 </x-app-layout>
+
 
 
 
