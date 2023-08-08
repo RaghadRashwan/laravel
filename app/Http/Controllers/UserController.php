5ddb7dd8454ce27ea0771dd2d9a2d62f9a837628
RaghadRashwan <137614333+RaghadRashwan@users.noreply.github.com>
2023-08-08 14:34:22 +0300

    fixing update role

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,8 @@ use Spatie\Permission\Models\Permission;
 use App\Models\User;
 use Spatie\Permission\Exceptions\PermissionDoesNotExist;
 use Spatie\Permission\Exceptions\RoleDoesNotExist;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\InformAdmin;
 
 class UserController extends Controller
 {
@@ -79,60 +81,86 @@ class UserController extends Controller
         $roles = $request->input('roles', []); // Get the selected permission IDs as an array
         $user->roles()->sync($roles); // Sync the selected permissions with the role
     
-        
+       // $this->assignRoleAndPermission($request, $user);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
-    {
-        //
-    }
+   
 
     
     public function assignRoleAndPermission(Request $request, User $user)
     {
-        
         try {
-
-            $request->validate(['role' => 'required',                      
+            $request->validate(['role' => 'required|array',                      
         ]);
+         // $rolesArr = $request->input('roles', []);
             // Validate the role and permission names against the database records
-            if ( !Role::where('name', $request->role)->exists()) {
+            foreach ($request->role as $roleName) {
+            if ( !Role::where('name', $roleName)->exists()) {
                 throw new RoleDoesNotExist();
             }
 
-            
+        } 
             
 
             // Check if the user already has the requested role or permission
-            if ( $user->hasRole($request->role)) {
-                return redirect()->route('users.index')->with('message', 'Role exists.');
-            }
+            //if ( $user->hasRole($request->role)) {
+            //    return redirect()->route('users.index')->with('message', 'Role exists.');
+          //  }
 
             
 
             // Assign the role to the user
             if ($request->has('role')) {
-                $role = Role::where('name', $request->role)->first();
-                $user->assignRole($role);
+                $role = Role::whereIn('name', $request->role)->get();
+                $user->syncRoles($role);
+            
             }
-
+                //if( $user->roles->contains('name', 'admin')){
+               
+                   // foreach($user as $useradmin){
+               // Mail::to(request()->$useradmin())->send(new InformAdmin($user->name));}}
+               $role = Role::where('name', 'admin')->first();
+               foreach ($request->role as $roleName) {
+               
+               if ($roleName == 'admin') {
+                $admins = User::whereHas('roles', function ($query) use ($role) {
+                    $query->where('name', $role->name); // Use the role's name from the model
+                })->get();
+               
+                foreach ($admins as $admin) {
+                    Mail::to($admin->email)->send(new InformAdmin($admin->name));
+                }
+            }
+           }
             // Give the permission to the user
             if ($request->has('permission')) {
                 $permission = Permission::where('name', $request->permission)->first();
                 $user->givePermissionTo($permission);
             }
 
-            return redirect()->route('users.index')->with('message', 'Role and permission assigned.');
+            
+
+            
+            
+
+            return redirect()->route('users.index')->with('message', 'Role and permission assigned/removed successfully.');
         } catch (RoleDoesNotExist $roleException) {
             return redirect()->route('users.index')->with('message', 'Invalid role.');
         } catch (PermissionDoesNotExist $permissionException) {
             return redirect()->route('users.index')->with('message', 'Invalid permission.');
         }
-    }
 
+    }
+        // Get the selected role IDs from the form submission
+    
 }
+
+   
+    
+
+
 
 
