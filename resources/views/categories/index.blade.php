b89edfa2658318d766e54ebce06991426bf0c7f8
RaghadRashwan <137614333+RaghadRashwan@users.noreply.github.com>
2023-08-13 10:23:34 +0300

    fixing styling in the category page

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -9,26 +9,24 @@
     </x-slot>
 
     <div class="py-12 flex justify-center">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+        <div class="w-screen h-full my-8 mx-8 sm:px-6 lg:px-8 bg-white">
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+            
                     @can('create category')
                    
                     <a href="{{ route('categories.create') }}" class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg shadow hover:bg-blue-600">Add new category</a>
                     @endcan
                     
                     <br /><br />
-                    <div class="container">
-                        <div class="flex flex-wrap -mx-4 justify-center">
-                            <!-- Blog entries-->
-                            <div class="w-full lg:w-2/3 px-4">
-                                <!-- Nested row for non-featured blog posts-->
-                                <div class="flex flex-wrap -mx-4">
+                    <div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 gap-1">
+                        
                                     @foreach($categories as $category)
-                                    <div class="w-full lg:w-1/2 px-4 mb-8">
+                                    <div class="flex flex-wrap -mx-4">
+
+                                        <div class="w-full  px-4 mb-8">
                                         <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                             <h2 class="mb-4 text-lg font-semibold tracking-tight text-gray-900 dark:text-white">{{ $category->name }}</h2>
-                                            <div class="flex justify-between items-center">
+                                            
                                                 @can('edit category')
                                                 @if(Auth::id() == optional($category->user)->id || auth()->user()->roles->contains('name', 'admin'))
                                                 <a href="{{ route('categories.edit', $category) }}" class="text-sm font-bold text-blue-500 hover:underline">Edit</a>
@@ -52,9 +50,9 @@
                                     @endforeach
                                 </div>
                             </div>
-                        </div>
+                        
                     </div>
-                </div>
+                
             </div>
         </div>
     </div>
