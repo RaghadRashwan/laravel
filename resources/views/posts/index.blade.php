f50e9b275dc4d1021e626cbf3ecc909c1d335108
RaghadRashwan <137614333+RaghadRashwan@users.noreply.github.com>
2023-08-13 10:26:28 +0300

    fixing styling of the post page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,8 +6,8 @@
     </x-slot>
 
     <div class="py-12 flex justify-center">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="w-screen h-full my-8 mx-8 sm:px-6 lg:px-8 bg-white">
+            
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     @can('create post')
                     <a href="{{ route('posts.create') }}" class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-lg shadow hover:bg-blue-600">
@@ -15,19 +15,17 @@
                     </a>
                     @endcan
                     <br /><br />
-                    <div class="container">
-                        <div class="flex flex-wrap -mx-4 justify-center">
-                            <!-- Blog entries-->
-                            <div class="w-full lg:w-2/3 px-4">
+                    <div class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 gap-1">
                                 <!-- Nested row for non-featured blog posts-->
-                                <div class="flex flex-wrap -mx-4">
-                                    @foreach($posts as $post)
-                                    <div class="w-full lg:w-1/2 px-4 mb-8">
+                                    @foreach($posts as $post)                                
+                                    <div class="flex flex-wrap -mx-4">
+
+                                    <div class="w-full  px-4 mb-8">
                                         <!-- Blog post-->
-                                        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                        <div class="max-w-sm bg-white border  border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                                             <!-- Display the image using the 'asset()' helper -->
                                             <a href="#">
-                                                <img class="rounded-t-lg" src="{{ asset('storage/'. $post->image ) }}" alt="...">
+                                                <img class="rounded-t-lg w-16 md:w-32 lg:w-48" src="{{ asset('storage/'. $post->image ) }}" alt="...">
                                             </a>
                                             <div class="p-5">
                                                 <!-- The rest of the post content -->
@@ -38,11 +36,11 @@
                                                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $post->post_text }}</p>
                                                 <a href="{{ route('posts.show', $post->id) }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                     Read more
-                                                     <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                                                     </svg>
                                                 </a>
-                                                
+
                                                 @can('edit post')
                                                 @if(Auth::id() == optional($post->user)->id || auth()->user()->roles->contains('name', 'admin'))
                                                 <a href="{{ route('posts.edit', $post) }}" class="inline-flex items-center px-3 py-2 mt-2 text-sm font-medium text-white bg-green-500 rounded-lg shadow hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
@@ -50,7 +48,7 @@
                                                 </a>
                                                 @endcan
                                                 @endif
-                                                
+
                                                 @can('delete post')
                                                 @if(Auth::id() == optional($post->user)->id || auth()->user()->roles->contains('name', 'admin'))
                                                 <form method="POST" action="{{ route('posts.destroy', $post) }}" style="display: inline-block;">
@@ -61,21 +59,22 @@
                                                     </button>
                                                 </form>
                                                 @endcan
-                        
+
                                                 @endif
                                             </div>
                                         </div>
-                                    </div>
+                                    </div></div>
                                     @endforeach
-                                </div>
+                                
                             </div>
-                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+
 
 
 
